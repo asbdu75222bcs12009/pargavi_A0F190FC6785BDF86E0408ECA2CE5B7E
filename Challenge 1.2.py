4fec7d96cadd453e68167891f4849f6e587f3f45
asbdu75222bcs12009 <144306960+asbdu75222bcs12009@users.noreply.github.com>
2023-11-01 10:12:45 +0530

    Delete Challenge 1.2.py

--- Challenge 1.2.py
+++
@@ -1,7 +0,0 @@
-def isleapyear(year):
-    if (year%4==0):
-      print('{}is not a leap year'.format (year))
-    else:
-      print('{} is not a leap year'.format (year))
-year=int(input("Enter a year value"))
-isleapyear(year)
